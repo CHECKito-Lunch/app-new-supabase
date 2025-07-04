8d43e12eb3ba00fac7d1949c19ff0618aafb1ff2
CHECKito-Lunch <c.friedrich89@gmail.com>
2025-07-04 10:45:38 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://qwfhwpwxouikqnfrkttl.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3Zmh3cHd4b3Vpa3FuZnJrdHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NTYwNzYsImV4cCI6MjA2NzEzMjA3Nn0.L45EhLH1ZRtUZbRMe5kevp9qk-oBZxz59MI7cz-O438'
+)
