20fc53107b59ff682816ddae11806ed9b7e5712e
CHECKito-Lunch <c.friedrich89@gmail.com>
2025-07-06 15:16:51 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,5 +2,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   'https://fsdomiwtzdhkxwpbbznc.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZG9taXd0emRoa3h3cGJiem5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MDMyMDAsImV4cCI6MjA2NzM3OTIwMH0.sePUOzZzO3A2qmjozyFHtNbnqWgQqPJyXzO0helr8UQ'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZG9taXd0emRoa3h3cGJiem5jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTgwMzIwMCwiZXhwIjoyMDY3Mzc5MjAwfQ.e0QzYktM4dCkX7CM2l3LoF5laj28Fg_ceDdeX4W34gw'
 )
