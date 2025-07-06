fd973622c08e5c276b5f9650c094fcda9d430dd3
CHECKito-Lunch <c.friedrich89@gmail.com>
2025-07-06 15:24:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+// 🔑 Deine Supabase Daten eintragen
+const supabaseUrl = 'https://fsdomiwtzdhkxwpbbznc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZG9taXd0emRoa3h3cGJiem5jIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MTgwMzIwMCwiZXhwIjoyMDY3Mzc5MjAwfQ.e0QzYktM4dCkX7CM2l3LoF5laj28Fg_ceDdeX4W34gw'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// Für Globalzugriff auf supabase:
+window.supabase = supabase
 
 
 const days = ['Montag','Dienstag','Mittwoch','Donnerstag','Freitag']
